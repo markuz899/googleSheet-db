320b31850061aca31debd58cd27cf42563265656
marco <markuz.89@hotmail.it>
2021-10-24 17:13:04 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Form, Container, Header } from "semantic-ui-react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+const axios = require("axios");
+const URL =
+  "https://sheet.best/api/sheets/442b63fc-1b8d-462c-9fcb-ea80c27487b9";
 
 function App() {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [state, setState] = useState({
+    name: "",
+    age: "",
+    salary: "",
+    hobby: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(state);
+    axios.post(URL, state).then((response) => {
+      setIsSuccess(true);
+      console.log(response);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="container">
+      <Header as="h2">React Google Sheets!</Header>
+      <Form className="form" onSubmit={submitHandler}>
+        <Form.Field>
+          <label>Name</label>
+          <input
+            placeholder="Inserisci nome"
+            type="text"
+            name="firstname"
+            defaultValue={state.name}
+            onChange={changeHandler}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Cognome</label>
+          <input
+            placeholder="Inserisci cognome"
+            type="text"
+            name="lastname"
+            defaultValue={state.age}
+            onChange={changeHandler}
+          />
+        </Form.Field>
+        <div className="column">
+          {isSuccess && "Dati inseriti"}
+          <Button color="blue" type="submit">
+            Invia
+          </Button>
+        </div>
+      </Form>
+    </Container>
   );
 }
 
